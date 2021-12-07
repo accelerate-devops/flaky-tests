2bfb5247d7ad864c9a3cf7ffb96e5805ad901d2b
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2021-12-07 10:03:17 +0100

    Update FlakyTests.cs

--- FlakyTests/FlakyTests.cs
+++ FlakyTests/FlakyTests.cs
@@ -9,16 +9,16 @@ namespace FlakyTests
         public void FlakyTest()
         {
             var random = new Random(DateTime.Now.Millisecond);
-            var next = random.Next(10);
-            Assert.True(next != 1, "Randomly failing test.");
+            var next = random.Next(8);
+            Assert.True(next % 3 != 0, "Randomly failing test.");
         }
 
         [Fact]
         public void AnotherFlakyTest()
         {
             var random = new Random(DateTime.Now.Millisecond);
-            var next = random.Next(10);
-            Assert.True(next != 2, "Another randomly failing test.");
+            var next = random.Next(8);
+            Assert.True(next % 3 != 0, "Another randomly failing test.");
         }
     }
 }
